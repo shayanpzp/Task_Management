bcd915482252be085005e353389aade61ef0ef2e
shayanpzp <shayanpzp1@gmail.com>
2023-12-17 18:26:57 +0330

    fix : fixing wsgi.py

--- Src/Src/wsgi.py
+++ Src/Src/wsgi.py
@@ -7,18 +7,10 @@ For more information on this file, see
 https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 """
 
-import imp
 import os
-import sys
 
-sys.path.insert(0, os.path.dirname(__file__))
+from django.core.wsgi import get_wsgi_application
 
-wsgi = imp.load_source('wsgi', 'dproj/wsgi.py')
-application = wsgi.application
-# import os
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Src.settings')
 
-# from django.core.wsgi import get_wsgi_application
-
-# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Src.settings')
-
-# application = get_wsgi_application()
+application = get_wsgi_application()
